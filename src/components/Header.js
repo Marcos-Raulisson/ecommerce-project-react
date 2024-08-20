417c671569ed891a17710e3db59da8eceaa08ea5
Marcos Raulisson <marcosraulisson@gmail.com>
2024-08-19 22:10:21 -0300

    Começo da Criação do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,40 +8,46 @@ import { faHeart, faUser } from '@fortawesome/free-regular-svg-icons';
 function Header (){
     return (
         <header className="App-header">
-        <div className='above-header'>
-          <div className='donwload-app'>
-            <p>Download App</p>
-            <FontAwesomeIcon icon={faWindows}/>
-            <FontAwesomeIcon icon={faGooglePlay}/>
-            <FontAwesomeIcon icon={faApple}/>
-          </div>
-          <p>Cupons de desconto de até <bold>90% de desconto</bold> na primeira compra</p>
-          <div className='sub-setup'>
-            <div className='dark-clear-mode'>
-              <p>Modo Escuro</p>
-              <FontAwesomeIcon icon={faMoon}/>
+            <div className='above-header'>
+            <div className='download-app'>
+                <p>Download App</p>
+                <div>
+                    <FontAwesomeIcon icon={faWindows}/>
+                    <FontAwesomeIcon icon={faGooglePlay}/>
+                    <FontAwesomeIcon icon={faApple}/>    
+                </div>
+                
             </div>
-            <div className='language-choice'>
-              <p>PT-BR</p>
+            <p>Cupons de desconto de até <b>90% de desconto</b> na primeira compra</p>
+            <div className='sub-setup'>
+                <div className='dark-clear-mode'>
+                <p>Modo Escuro</p>
+                <FontAwesomeIcon icon={faMoon}/>
+                </div>
+                <div className='language-choice'>
+                <p>PT-BR</p>
+                </div>
             </div>
-          </div>
-        </div>
-        <div className='main-tool-bar'>
-          <div className='logo'>
-            StoreWay
-          </div>
-          <input></input>
-          <FontAwesomeIcon icon={faHeart}/>
-          <FontAwesomeIcon icon={faCartShopping}/>
-          <FontAwesomeIcon icon={faUser}/>
-        </div>
-        <div className='sub-links'>
-          <a href='#'><FontAwesomeIcon icon={faCamera}/>Câmeras e Acessórios<FontAwesomeIcon icon={faAngleDown}/></a>
-          <a href='#'><FontAwesomeIcon icon={faMobileScreen}/>Celulares e Telefones<FontAwesomeIcon icon={faAngleDown}/></a>
-          <a href='#'><FontAwesomeIcon icon={faDesktop}/>Eltrônicos<FontAwesomeIcon icon={faAngleDown}/></a>
-          <a href='#'><FontAwesomeIcon icon={faRing}/>Jóias e Relógios<FontAwesomeIcon icon={faAngleDown}/></a>
-          <a href='#'><FontAwesomeIcon icon={faDumbbell}/>Saúde<FontAwesomeIcon icon={faAngleDown}/></a>
-        </div>
+            </div>
+            <div className='main-tool-bar'>
+                <div className='logo'>
+                    <b>Store</b>Way
+                </div>
+                <input></input>
+                <div className='menu-icons'>
+                    <FontAwesomeIcon icon={faHeart}/>
+                    <FontAwesomeIcon icon={faCartShopping}/>
+                    <FontAwesomeIcon icon={faUser}/>            
+                </div>
+                
+            </div>
+            <div className='sub-links'>
+            <a href='#'><FontAwesomeIcon icon={faCamera}/>Câmeras e Acessórios<FontAwesomeIcon icon={faAngleDown}/></a>
+            <a href='#'><FontAwesomeIcon icon={faMobileScreen}/>Celulares e Telefones<FontAwesomeIcon icon={faAngleDown}/></a>
+            <a href='#'><FontAwesomeIcon icon={faDesktop}/>Eltrônicos<FontAwesomeIcon icon={faAngleDown}/></a>
+            <a href='#'><FontAwesomeIcon icon={faRing}/>Jóias e Relógios<FontAwesomeIcon icon={faAngleDown}/></a>
+            <a href='#'><FontAwesomeIcon icon={faDumbbell}/>Saúde<FontAwesomeIcon icon={faAngleDown}/></a>
+            </div>
       </header>
     );
 }
